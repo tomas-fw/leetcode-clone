f1f323cd238920bead061d446c7e1242d72ec9a1
tomwilson89 <tfuenteswilson@gmail.com>
2023-09-26 21:56:16 -0300

    chore: add two sum and reverse link list problems

--- src/app/problems/components/Playground/index.tsx
+++ src/app/problems/components/Playground/index.tsx
@@ -7,13 +7,17 @@ import PreferencesNav from '../PreferenceNav';
 type Props = {};
 
 const Playground = (props: Props) => {
+    const boilerplate = `
+    function twoSum(nums, target) {
+        // Your code here
+    };`;
     return (
-        <div className='flex flex-col bg-dark-layer-1 relative'>
+        <div className='flex flex-col bg-dark-layer-1 relative overflow-x-hidden'>
             <PreferencesNav />
             <Split className='h-[calc(100vh-94px)]' direction='vertical' sizes={[61, 40]} minSize={60}>
                 <div className='w-full overflow-auto'>
                     <CodeMirror
-                        value='const a = 1;'
+                        value={boilerplate}
                         theme={vscodeDark}
                         extensions={[javascript()]}
                         style={{ fontSize: 16 }}
