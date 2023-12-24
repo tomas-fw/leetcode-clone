f8ec485f5eb0c960b1238b9d9e840b089489fa0c
tomwilson89 <tfuenteswilson@gmail.com>
2023-12-24 17:57:33 +0100

    feat: add confeti lib

--- src/app/problems/components/Workspace/index.tsx
+++ src/app/problems/components/Workspace/index.tsx
@@ -1,6 +1,11 @@
 'use client';
 
+import { If } from '@/components/flow-control';
+import useHasMounted from '@/hooks/useHasMounted';
+import useWindowSize from '@/hooks/useWindowSize';
 import { Problem, ProlemTable } from '@/types/problem';
+import { useState } from 'react';
+import Confetti from 'react-confetti';
 import Split from 'react-split';
 import Playground from '../Playground';
 import ProblemDescription from '../ProblemDescription';
@@ -10,11 +15,21 @@ type Props = {
     problemData: ProlemTable;
 };
 const Workspace = ({ problem, problemData }: Props) => {
+    const { width, height } = useWindowSize();
+    const hasMounted = useHasMounted();
+    const [success, setSuccess] = useState(false);
+
+    if (!hasMounted) return null;
     return (
-        <Split className='split' minSize={0}>
-            <ProblemDescription problem={problem} problemData={problemData} />
-            <Playground problem={problem} />
-        </Split>
+        <>
+            <If condition={success}>
+                <Confetti gravity={0.3} tweenDuration={4000} width={width} height={height} />
+            </If>
+            <Split className='split' minSize={0}>
+                <ProblemDescription problem={problem} problemData={problemData} />
+                <Playground problem={problem} />
+            </Split>
+        </>
     );
 };
 
---
+++ src/hooks/useWindowSize.ts
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+
+type WindowSize = {
+    width: number | undefined;
+    height: number | undefined;
+};
+
+const useWindowSize = () => {
+    const [windowSize, setWindowSize] = useState<WindowSize>({
+        width: undefined,
+        height: undefined,
+    });
+
+    useEffect(() => {
+        // Handler to call on window resize
+        const handleResize = () => {
+            // Set window width and height to state
+            setWindowSize({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
+        };
+
+        // Add event listener
+        window.addEventListener('resize', handleResize);
+
+        // Call handler right away so state gets updated with initial window size
+        handleResize();
+
+        // Remove event listener on cleanup
+        return () => window.removeEventListener('resize', handleResize);
+    }, []); // Empty array ensures that effect runs only on mount and unmount
+
+    return windowSize;
+};
+
+export default useWindowSize;
